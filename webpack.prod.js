c0c31741d90845ef621a1c9386a8baa9388a9d75
kenneth-jiang <kennethjiang1024@gmail.com>
2021-10-15 18:59:56 -0400

    Updated output

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js',
+        filename: '[name].js',
         publicPath: './',
     },
     module: {
