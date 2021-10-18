da995c47ed878309e19d188d60b0e76bcbeba690
kenneth-jiang <kennethjiang1024@gmail.com>
2021-10-18 12:00:33 -0400

    Fixed path.

--- src/api/lunarcrushApi.js
+++ src/api/lunarcrushApi.js
@@ -3,7 +3,8 @@ import request from './request';
 const baseUrl = "https://api.lunarcrush.com";
 
 export const getLunarcrushGlobalData = () => {
-    return request.get(baseUrl + "/v2?data=global&interval=day&change=1d&data_points=30&key=" + process.env.LUNARCRUSH_API_KEY);
+    return request.get("/lunarcrush/globaldata");
+    // return request.get(baseUrl + "/v2?data=global&interval=day&change=1d&data_points=30&key=" + process.env.LUNARCRUSH_API_KEY);
 };
 
 export const getLunarcrushCoinOfTheDay = () => {
